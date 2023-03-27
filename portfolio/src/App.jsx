6912e806468df9056919f999db4122f882202c0c
bouduben <bouduben@gmail.com>
2023-03-27 22:26:28 +0200

    sliding page1

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,6 +1,6 @@
-import React from "react";
-import { useEffect } from "react";
-import { Outlet } from "react-router-dom";
+import { useEffect, useState, useContext } from "react";
+import { SlideContext } from "./index";
+import { Outlet, useNavigate } from "react-router-dom";
 import { SidebarRight, SidebarLeft } from "./components/Sidebar/Sidebar";
 import Home from "./pages/Home/Home";
 import ProjectsPage from "./pages/ProjectsPage/ProjectsPage";
@@ -11,7 +11,51 @@ import "./App.scss";
 /**
  * @returns 3 Horizontals slides Home, Projects, About and right and left sidebars.
  */
-export default function App() {
+export default function App(event) {
+    /* context state to track current slide (horizontal and vertical) */
+    const { currentSlide } = useContext(SlideContext);
+
+    const navigate = useNavigate();
+
+    const [touchPos, setTouchPos] = useState({
+        originX: null,
+        originY: null
+    });
+
+    const handleTouchStart = (event) => {
+        setTouchPos({
+            originX: event.touches[0].clientX,
+            originY: event.touches[0].clientY
+        });
+    };
+    const handleTouchMove = (event) => {
+        if (event.touches[0].clientX - touchPos.originX > 200) {
+        }
+        if (event.touches[0].clientX - touchPos.originX < -200) {
+            nextPage();
+        }
+        if (event.touches[0].clientX - touchPos.originY > 200) {
+        }
+        if (event.touches[0].clientX - touchPos.originY < -200) {
+        }
+    };
+
+    const nextPage = () => {
+        switch (currentSlide.h) {
+            case 0:
+                navigate("/works");
+                break;
+            case 1:
+                navigate("/about");
+                break;
+            case 2:
+                navigate("/");
+                break;
+            default:
+                break;
+        }
+    };
+
     /**
      * function to stop css trasition during the window rezize
      */
@@ -28,7 +72,10 @@ export default function App() {
         return () => window.removeEventListener("resize", handleResize);
     });
     return (
-        <main>
+        <main
+            onTouchStart={(event) => handleTouchStart(event)}
+            onTouchMove={(event) => handleTouchMove(event)}
+        >
             <SidebarLeft />
             <SidebarRight />
             <HSlide key={1} index={0}>
